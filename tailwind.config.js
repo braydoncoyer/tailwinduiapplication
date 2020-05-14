1760a6a1f2f7d99f188e1c82f640e7acc4ed7291
Braydon Coyer <braydon@coyer.net>
2020-05-14 14:02:28 -0500

    hero section complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,23 @@ module.exports = {
       padding: '1rem'
     },
     borderWidth: {
-          default: '1px',
-          '0': '0',
-          '2': '2px',
-          '3': '3px',
-          '4': '4px',
-          '6': '6px',
-          '8': '8px',
-          '12': '12px',
-          '24': '24px'
-}
+      default: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+      '12': '12px',
+      '24': '24px'
+    },
+    extend: {
+      colors: {
+        primaryDark: '#333333',
+        primaryBg: '#ebebeb',
+        lightGray: '#cccccc',
+      },
+    }
   },
   module : {
     rules: [
