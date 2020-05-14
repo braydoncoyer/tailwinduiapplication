4b13ffee1c812864008632cde76d9af82f2d1afd
Braydon Coyer <braydon@coyer.net>
2020-05-14 13:04:14 -0500

    adding tailwind custom values

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,21 @@
 module.exports = {
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem'
+    },
+    borderWidth: {
+          default: '1px',
+          '0': '0',
+          '2': '2px',
+          '3': '3px',
+          '4': '4px',
+          '6': '6px',
+          '8': '8px',
+          '12': '12px',
+          '24': '24px'
+}
+  },
   module : {
     rules: [
       {
