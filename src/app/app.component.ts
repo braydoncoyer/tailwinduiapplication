4b713f8acf0a808c50462ee0bef8e040cb9287e4
Braydon Coyer <braydon@coyer.net>
2020-05-18 10:43:42 -0500

    Added fontawesome and added social icons to hero section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faCodepen } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tailwindapp';
+  faLinkedIn = faLinkedin;
+  faCodepen = faCodepen;
+  faEmail = faEnvelopeSquare;
 }
