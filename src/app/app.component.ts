59bc46022213b4d952801731b2d9dbf405e97534
Braydon Coyer <braydon@coyer.net>
2020-05-18 17:01:49 -0500

    added codepens to projects section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faCodepen } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,5 @@ export class AppComponent {
   faLinkedIn = faLinkedin;
   faCodepen = faCodepen;
   faEmail = faEnvelopeSquare;
+  faArrowDown = faAngleDown;
 }
